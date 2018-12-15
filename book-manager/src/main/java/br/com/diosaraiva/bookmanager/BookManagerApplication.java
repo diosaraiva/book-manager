08d406499eaa8987201a57d980c6d0b9c31b0912
User <User@Lenovo-T500>
2018-12-15 19:10:48 -0200

    Atualizações na modelagem e inclusão do documento de Walkthrough (InstrucoesProjeto.docx).

--- book-manager/src/main/java/br/com/diosaraiva/bookmanager/BookManagerApplication.java
+++ book-manager/src/main/java/br/com/diosaraiva/bookmanager/BookManagerApplication.java
@@ -1,8 +1,14 @@
 package br.com.diosaraiva.bookmanager;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+//Requisito 005: OK
 @SpringBootApplication
 public class BookManagerApplication {
 
@@ -10,5 +16,20 @@ public class BookManagerApplication {
 		SpringApplication.run(BookManagerApplication.class, args);
 	}
 
+	//Lista os Beans do servidor
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Beans do Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println("Bean: "+beanName);
+			}
+
+		};
+	}
 }
 
