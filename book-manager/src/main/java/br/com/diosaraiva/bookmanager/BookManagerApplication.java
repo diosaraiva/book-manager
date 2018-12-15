25b36c7cb41600f2a8f5699e973d995bef1c4b26
User <User@Lenovo-T500>
2018-12-14 23:25:09 -0200

    Primeiro commit do Projeto, com spring-boot-starter-web

---
+++ book-manager/src/main/java/br/com/diosaraiva/bookmanager/BookManagerApplication.java
@@ -0,0 +1,14 @@
+package br.com.diosaraiva.bookmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagerApplication.class, args);
+	}
+
+}
+
