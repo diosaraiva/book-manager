65015cd62d354b506e3e996585b4869fb9c4487f
User <User@Lenovo-T500>
2018-12-15 22:23:32 -0200

    Classes de teste Hello com JUnit OK.

---
+++ book-manager/src/main/java/br/com/diosaraiva/bookmanager/hello/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.diosaraiva.bookmanager.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
