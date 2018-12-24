ebddd35f55c2893360b1c7f6ddb56f272a01435b
User <diosaraiva@hotmail.com>
2018-12-24 00:02:42 -0200

    HelloController redefinido (TestController).

--- book-manager/src/main/java/br/com/diosaraiva/bookmanager/controller/TestController.java
+++ book-manager/src/main/java/br/com/diosaraiva/bookmanager/controller/TestController.java
@@ -1,6 +1,7 @@
 package br.com.diosaraiva.bookmanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,18 +9,25 @@ import br.com.diosaraiva.bookmanager.service.ILivroService;
 import br.com.diosaraiva.bookmanager.utils.LivroUtil;
 
 @RestController
-public class HelloController {
-	
+public class TestController {
+
 	@Autowired
 	private ILivroService livroService;
-	
+
 	//Mapeamento para raiz URI do servidor
 	@RequestMapping("/")
 	public String index() {
-		
-		livroService.adicionarListaLivros(LivroUtil.criarCenarioListaLivroTeste(20));
+
 		return "Servidor OK!";
-	
+
 	}
-	
+
+	@RequestMapping("/test/{quantidade}")
+	public String criarLivrosAleatorios(@PathVariable("quantidade") Integer quantidade) {
+
+		livroService.adicionarListaLivros(LivroUtil.criarCenarioListaLivroTeste(quantidade));
+
+		return quantidade.toString() + " livros aleatórios criados!";
+
+	}
 }
