56e168c1fc504234328af529be9504392fe76ce0
User <diosaraiva@hotmail.com>
2018-12-26 01:28:27 -0200

    Commit Frontend 2/3

--- book-manager-minimal/routes/index.js
+++ book-manager-minimal/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.sendFile(__dirname + './public/index.html');
 });
+
+/* DEFAULT GET home page.
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+}); */
 
 module.exports = router;
